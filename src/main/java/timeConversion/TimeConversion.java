717b4607c040bbdd65ad4042b2a8cec8268de813
aaali <aamir_ali@gap.com>
2022-05-19 16:59:39 +0500

    starting optimizations

--- src/main/java/timeConversion/TimeConversion.java
+++ src/main/java/timeConversion/TimeConversion.java
@@ -1,8 +1,11 @@
 package timeConversion;
 
+import org.jboss.logging.Logger;
+
 import java.text.DecimalFormat;
 
 public class TimeConversion {
+    static final Logger log = Logger.getLogger(TimeConversion.class);
 
     private static final DecimalFormat df = new DecimalFormat("00");
 
@@ -14,13 +17,16 @@ public class TimeConversion {
         merr = s.substring(s.length() - 2);
         int hour = Integer.parseInt(arr[0]);
 
-        h = merr.equalsIgnoreCase("AM") || hour == 12
-                ? hour : 12 + hour;
-        m = Integer.parseInt(arr[1]);
-        ss = Integer.parseInt(arr[2]);
-
+        h = hour;
+        if (merr.equalsIgnoreCase("AM") && hour == 12)
+            h = 0;
+        if (merr.equalsIgnoreCase("PM") && hour != 12)
+            h += 12;
         if (h > 23)
             h -= 24;
+
+        m = Integer.parseInt(arr[1]);
+        ss = Integer.parseInt(arr[2]);
 
         return (df.format(h) + ":" + df.format(m) + ":" + df.format(ss));
 
