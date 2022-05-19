e9ce257883c9c37856bd219a3e4dfab81793a14c
aaali <aamir00@hotmail.com>
2022-05-19 17:40:47 +0500

    optimizations on time conversion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         //Arrays.stream(input).forEach(a -> a%2 == 0 ? pos =+1 : odd +=1);
-        System.out.println(TimeConversion.timeConversion("0:45:54PM"));
+        System.out.println(TimeConversion.timeConversion("12:45:54PM"));
 
 
     }
--- src/main/java/timeConversion/TimeConversion.java
+++ src/main/java/timeConversion/TimeConversion.java
@@ -10,25 +10,21 @@ public class TimeConversion {
     private static final DecimalFormat df = new DecimalFormat("00");
 
     public static String timeConversion(String s) {
-        int h = 0, m = 0, ss = 0;
+        int h = 0;
         String[] arr = {};
-        String merr = "AM";
         arr = s.split("[a-zA-Z:]");
-        merr = s.substring(s.length() - 2);
+        String merr = s.substring(s.length() - 2);
         int hour = Integer.parseInt(arr[0]);
 
         h = hour;
-        if (merr.equalsIgnoreCase("AM") && hour == 12)
-            h = 0;
-        if (merr.equalsIgnoreCase("PM") && hour != 12)
-            h += 12;
-        if (h > 23)
-            h -= 24;
+        if (merr.equalsIgnoreCase("AM")) {
+            h %= 12;
+        } else {
+            h = h == 12 ? 12 : h + 12;
+        }
 
-        m = Integer.parseInt(arr[1]);
-        ss = Integer.parseInt(arr[2]);
 
-        return (df.format(h) + ":" + df.format(m) + ":" + df.format(ss));
+        return (df.format(h) + ":" + arr[1] + ":" + arr[2]);
 
     }
 
