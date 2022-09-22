76a09fbe7b6b9d660339a5fc0d1621afc9b2db4e
M Aamir Ali <aamir00@hotmail.com>
2022-09-22 21:16:39 +0500

    A few more

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import CountSort.CountingSort;
 import flippingBits.FlippingBits;
 import lonelyInteger.LonelyInteger;
 import org.jboss.logging.Logger;
+import pangram.Pangram;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,20 +17,8 @@ public class Main {
     private static final Logger log = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
-        List<Integer> i = new ArrayList<>();
-        i.add(1);
-        i.add(1);
-        i.add(2);
-        i.add(2);
-        i.add(3);
-        i.add(3);
-        i.add(16);
-        i.add(32);
-        i.add(16);
-        i.add(5);
-        i.add(32);
-        i.add(7);
-        System.out.println(CountingSort.countingSort(i));
+
+        System.out.println(Pangram.pangrams("NOVmETKPTbYu ftZPEykhjgF GGkdGjWGwW skjPJsea dtwdqcr DERCUgxOxrRgDQbdzL IZjyXs"));
 
     }
 
--- src/main/java/pangram/Pangram.java
+++ src/main/java/pangram/Pangram.java
@@ -1,4 +1,28 @@
 package pangram;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collector;
+
 public class Pangram {
+
+    public static String pangrams(String s) {
+
+        StringBuilder sb = new StringBuilder(s.toLowerCase().replaceAll("[^a-z^ ]+", ""));
+        List<Integer> uniqueList = new ArrayList<>();
+        sb.chars().distinct().forEach(uniqueList::add);
+        System.out.println(uniqueList.size());
+        if (uniqueList.size() == 27 )
+            return "Pangram";
+        else return
+                "Not Pangram";
+    }
+
 }
